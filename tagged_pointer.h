8ee8a7e86031f64dcd28f814d1a2b861fcf09ede
Neel <Neel012users.noreply.github.com>
2016-05-04 18:22:49 +0200

    tagged pointer

--- tagged_pointer.h
+++ tagged_pointer.h
@@ -3,30 +3,49 @@
 
 namespace lockfree {
 
-// TODO: determine which bits get used in address
-const std::uint64_t ptr_mask = 0xffffffffffffUL;
-const std::uint64_t counter_mask;
-const std::uint16_t counter_tag[4];
-
-union pointer_value {
-  uint64_t ptr;
-  uint16_t counter_tag[4];
-};
-
 template <typename T>
 struct tagged_pointer {
-  T* ptr() {
-    return ptr_mask & value_;
+  tagged_pointer(T* ptr, uint16_t count = 0) noexcept {
+    value_.ptr = uint64_t(ptr);
+    value_.counter_tag[3] = count;
   }
 
-  uint16_t count() {
-    pointer_value x;
-    x.ptr = value_;
-    return x.counter_tag[3];
+  T* ptr() const {
+    return (T*)(ptr_mask & value_.ptr);
+  }
+
+  uint16_t count() const {
+    return value_.counter_tag[3];
+  }
+
+  bool operator==(const tagged_pointer& rhs) {
+    return count() == rhs.count() && ptr() == rhs.ptr();
   }
 
 private:
-  uint64_t value_;
+  union pointer_value {
+    uint64_t ptr;
+    uint16_t counter_tag[4];
+  };
+
+  static const std::uint64_t ptr_mask = 0xffffffffffffUL;
+
+  pointer_value value_;
 };
+
+TEST_CASE("tagged_pointer - basic test") {
+  int i = 42;
+  tagged_pointer<int> tagptr(&i, 5);
+  REQUIRE(tagptr.count() == 5);
+  REQUIRE(tagptr.ptr() == &i);
+  REQUIRE(*tagptr.ptr() == i);
+}
+
+TEST_CASE("tagged_pointer - operator==") {
+  int i = 42;
+  tagged_pointer<int> a(&i, 5);
+  tagged_pointer<int> b(&i, 5);
+  REQUIRE(a == b);
+}
 
 }
