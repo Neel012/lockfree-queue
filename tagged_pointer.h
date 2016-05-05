15991dd0e8f7199d7161e9848f0b018f814e1c17
Neel <Neel012users.noreply.github.com>
2016-05-05 14:40:04 +0200

    make tagged_pointer default constructible

--- tagged_pointer.h
+++ tagged_pointer.h
@@ -7,6 +7,8 @@ namespace lockfree {
 
 template <typename T>
 struct tagged_pointer {
+  tagged_pointer() = default;
+
   tagged_pointer(T* ptr, uint16_t count = 0) noexcept {
     value_.ptr = uint64_t(ptr);
     value_.counter_tag[3] = count;
@@ -35,52 +37,47 @@ private:
   pointer_value value_;
 };
 
-TEST_CASE("tagged_pointer - basic test") {
-  SECTION("stack pointer") {
-    int i = 42;
+TEST_CASE("tagged_pointer - on stack") {
+  int i = 42;
+
+  SECTION("basic test") {
     tagged_pointer<int> tagptr(&i, 5);
     REQUIRE(tagptr.count() == 5);
     REQUIRE(tagptr.ptr() == &i);
     REQUIRE(*tagptr.ptr() == i);
   }
-  SECTION("heap pointer") {
-    int i = new int{42};
-    tagged_pointer<int> tagptr(i, 5);
-    REQUIRE(tagptr.count() == 5);
-    REQUIRE(tagptr.ptr() == i);
-    REQUIRE(*tagptr.ptr() == *i);
-    delete i;
-  }
-}
-
-TEST_CASE("tagged_pointer - size test") {
-  SECTION("stack pointer") {
-    int i = 42;
+  SECTION("size test") {
     tagged_pointer<int> a(&i, 5);
     REQUIRE(sizeof(a) == sizeof(&i));
   }
-  SECTION("heap pointer") {
-    int* i = new int{42};
-    tagged_pointer<int> a(i, 5);
-    REQUIRE(sizeof(a) == sizeof(i));
-    delete i;
-  }
-}
-
-TEST_CASE("tagged_pointer - operator==") {
-  SECTION("stack pointer") {
-    int i = 42;
+  SECTION("operator==") {
     tagged_pointer<int> a(&i, 5);
     tagged_pointer<int> b(&i, 5);
     REQUIRE(a == b);
   }
-  SECTION("heap pointer") {
-    int i = new int{42};
+}
+
+TEST_CASE("tagged_pointer - on heap") {
+  int* i = new int{42};
+
+  SECTION("basic test") {
+    int* i = new int{42};
+    tagged_pointer<int> tagptr(i, 5);
+    REQUIRE(tagptr.count() == 5);
+    REQUIRE(tagptr.ptr() == i);
+    REQUIRE(*tagptr.ptr() == *i);
+  }
+  SECTION("size test") {
+    int* i = new int{42};
+    tagged_pointer<int> a(i, 5);
+    REQUIRE(sizeof(a) == sizeof(i));
+  }
+  SECTION("operator==") {
     tagged_pointer<int> a(i, 5);
     tagged_pointer<int> b(i, 5);
     REQUIRE(a == b);
-    delete i;
   }
+  delete i;
 }
 
 }
