c1ffe30ddf2a05354a88e1485b5ebd0227736ec3
Neel <Neel012@users.noreply.github.com>
2016-05-09 19:37:27 +0200

    garbage_list's test

--- list.h
+++ list.h
@@ -1,5 +1,6 @@
 #include <atomic>
 #include <memory>
+#include "tests/catch.hpp"
 
 namespace lockfree {
 
@@ -9,7 +10,7 @@ struct list {
   struct node {
     T data;
     //std::unique_ptr<node> next;
-    node* next;
+    node* next = nullptr;
   };
 
   // Assumes rhs is not beeing accessed concurently
@@ -36,10 +37,10 @@ struct list {
   //  }
   //}
 
+  // TODO: make sure unique_ptr takes ownership of passed pointer
   template <typename... Args>
   void emlpace_back(Args&&... args) {
-    //auto new_node = std::make_unique<node>(std::forward<Args>(args)..., nullptr);
-    auto new_node = new node{std::forward<Args>(args)..., nullptr};
+    auto new_node = new node{std::forward<Args>(args)...};
     if (tail_ == nullptr) {
       tail_ = new_node;
       head_.store(new_node);
@@ -64,5 +65,30 @@ private:
   std::atomic<node*> head_ {nullptr};
   node* tail_ {nullptr};
 };
+
+namespace {
+
+struct test_counter {
+  static int n;
+
+  test_counter() { n++; }
+
+  ~test_counter() { n--; }
+};
+
+int test_counter::n{0};
+
+}
+
+TEST_CASE("Garbage List - Basic test") {
+  SECTION("Proper deallocation") {
+    list<test_counter> l;
+    for (int i{0}; i < 20; i++) {
+      l.emlpace_back();
+    }
+    l.clear();
+    REQUIRE(test_counter::n == 0);
+  }
+}
 
 }
