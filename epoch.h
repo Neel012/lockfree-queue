d192167b4416a2c4ff5bd4d75763d51c419c2292
Neel <Neel012@users.noreply.github.com>
2016-05-09 19:00:52 +0200

    Redesigned guard interface to work as std::lock_guard

--- epoch.h
+++ epoch.h
@@ -8,57 +8,61 @@
 
 namespace lockfree {
 
+static constexpr unsigned epoch_count{3};
+
+template <typename T>
+struct epoch;
+
+template <typename T>
+struct guard {
+  using epoch = epoch<T>;
+  using limbo_list = typename epoch::limbo_list;
+
+  guard(epoch& e) noexcept : e_{e}, thread_epoch{e.global.load()} {
+    e_.active[thread_epoch % epoch_count]++; // observe the current epoch
+    if (e_.all_observed_epoch(thread_epoch) &&
+        e_.global.compare_exchange_strong(thread_epoch, thread_epoch + 1))
+    {
+      e_.free_epoch(thread_epoch - 1);
+    }
+  }
+
+  ~guard() noexcept {
+    if (!unpinned) {
+      unpin();
+    }
+  }
+
+  void unlink(T* pointer) {
+    e_.local_unlinked[thread_epoch % epoch_count].emplace_front(pointer);
+  }
+
+  void unpin() {
+    // fixme: leaking some memory at the end of a lifetime of the epoch object
+    // ... deallocation takes place at the begining of the guarded operation
+    e_.active[thread_epoch % epoch_count]--;
+    e_.merge_garbage(local_unlinked, thread_epoch);
+    unpinned = true;
+  }
+
+private:
+  /* data */
+  epoch& e_;
+  unsigned thread_epoch;
+  limbo_list local_unlinked;
+  bool unpinned{false};
+};
+
 // epoch manages pointers T
 template <typename T>
 struct epoch {
-  static constexpr unsigned epoch_count{3};
-
   //using limbo_list = std::forward_list<std::unique_ptr<T>>;
   using limbo_list = list<std::unique_ptr<T>>;
   //using limbo_list = list<T*>;
   using epoch_garbage = std::array<limbo_list, epoch_count>;
 
-  struct guard {
-    guard(epoch& e) noexcept : e_{e}, thread_epoch{e.global.load()} {
-      e_.active[thread_epoch % epoch_count]++; // observe the current epoch
-      if (e_.all_observed_epoch(thread_epoch) &&
-          e_.global.compare_exchange_strong(thread_epoch, thread_epoch + 1))
-      {
-        e_.free_epoch(thread_epoch - 1);
-      }
-    }
-
-    ~guard() noexcept {
-      if (!unpinned) {
-        unpin();
-      }
-    }
-
-    void unlink(T* pointer) {
-      e_.local_unlinked[thread_epoch % epoch_count].emplace_front(pointer);
-    }
-
-    void unpin() {
-      // fixme: leaking some memory at the end of a lifetime since deallocation
-      // takes place at the begining of the guarded operation
-      e_.active[thread_epoch % epoch_count]--;
-      e_.merge_garbage(local_unlinked, thread_epoch);
-      unpinned = true;
-    }
-
-  private:
-    epoch& e_;
-    unsigned thread_epoch;
-    limbo_list local_unlinked;
-    bool unpinned{false};
-  };
-
-  guard pin() {
-    return guard{*this};
-  }
-
 private:
-  friend guard;
+  friend guard<T>;
 
   void free_epoch(unsigned n) {
     unlinked[n % epoch_count].clear();
@@ -72,6 +76,7 @@ private:
     unlinked[local_epoch % epoch_count].merge(local_unlinked);
   }
 
+  /* data */
   std::atomic<unsigned> global{1};
   std::array<std::atomic<unsigned>, epoch_count> active;
   epoch_garbage unlinked;
@@ -81,13 +86,14 @@ private:
 TEST_CASE("Epoch - Basic test") {
   epoch<int> e;
   SECTION("") {
-    auto g = e.pin();
+    //auto g = e.pin();
   }
   SECTION("") {
-    epoch<int>::guard g = e.pin();
+    //epoch<int>::guard g = e.pin();
   }
   SECTION("") {
-    epoch<int>::guard g{e};
+    //epoch<int>::guard g{e};
+    guard<int> g{e};
   }
 }
 
