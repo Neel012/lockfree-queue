c016ac3459967564f77864c8245a7ff532e7e8dc
lamanchy <lamanchy@gmail.com>
2016-05-15 13:21:10 +0200

    move tests (doesn't work when doing something else then tests)

--- tests/common.cpp
+++ tests/common.cpp
@@ -2,3 +2,19 @@
 #include "catch.hpp"
 #include "../ms_queue.hpp"
 #include "../epoch_queue.hpp"
+
+namespace lockfree {
+namespace tests {
+
+TEST_CASE("Basic tests - single thread") {
+  ms_queue<int> q;
+  q.enqueue(1);
+  q.enqueue(2);
+  q.enqueue(3);
+  REQUIRE(*q.dequeue() == 1);
+  REQUIRE(*q.dequeue() == 2);
+  REQUIRE(*q.dequeue() == 3);
+}
+
+} // namespace tests
+} // namespace lockfree
