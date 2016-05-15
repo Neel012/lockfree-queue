6d988279536beee2b21cd7661adf3f4d52317348
Neel <Neel012@users.noreply.github.com>
2016-05-15 06:29:41 +0200

    any_ptr: manages type-erased pointers

---
+++ any_ptr.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <utility>
+
+// Type erased smart pointer
+struct any_ptr {
+  using pointer_type = void*;
+  using deleter_type = void(pointer_type);
+
+  template <typename E>
+  struct default_delete {
+    void operator()(void* ptr) {
+      delete static_cast<E*>(ptr);
+    }
+  };
+
+  any_ptr() = default;
+
+  any_ptr(any_ptr&& rhs) noexcept
+      : pointer_{std::move(rhs.pointer_)}, deleter_{std::move(rhs.deleter_)}
+  {
+    rhs.pointer_ = nullptr;
+  }
+
+  // any_ptr(pointer_type pointer, std::function<deleter_type>&& deleter) noexcept
+  //     : pointer_{pointer}, deleter_{std::move(deleter)}
+  // { }
+
+  template <typename E>
+  any_ptr(
+      E* pointer,
+      std::function<deleter_type>&& deleter = default_delete<E>{}) noexcept
+      : pointer_{pointer}, deleter_{std::move(deleter)}
+  { }
+
+  ~any_ptr() noexcept {
+    reset();
+  }
+
+  void reset() noexcept {
+    if (pointer_ != nullptr) {
+      deleter_(pointer_);
+      pointer_ = nullptr;
+    }
+  }
+
+  pointer_type get() const {
+    return pointer_;
+  }
+
+  pointer_type operator->() const {
+    return get();
+  }
+
+  bool operator==(const any_ptr& rhs) {
+    return get() == rhs.get();
+  }
+
+  bool operator==(std::nullptr_t) {
+    return get() == nullptr;
+  }
+
+private:
+  pointer_type pointer_{nullptr};
+  std::function<deleter_type> deleter_;
+};
+
+// template <typename E>
+// any_ptr make_any_ptr(E* ptr, std::function<any_ptr::deleter_type>&& deleter =
+//                                  any_ptr::default_delete<E>{}) noexcept
+// {
+//   return any_ptr{ptr, std::move(deleter)};
+// }
