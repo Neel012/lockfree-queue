dc8dd9c5045ed108555e4d11d62a424c7311b250
Neel <Neel012@users.noreply.github.com>
2016-05-15 21:02:00 +0200

    epoch_queue: more tests

--- tests/epoch_queue.cpp
+++ tests/epoch_queue.cpp
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_MAIN
-#include "../epoch_queue.hpp"
+#include <future>
+#include <thread>
 #include "catch.hpp"
+#include "../epoch_queue.hpp"
 
 namespace lockfree {
 
@@ -9,13 +11,64 @@ TEST_CASE("epoch_queue - single thread") {
     epoch_queue<int> q;
     REQUIRE(q.empty());
     q.enqueue(1);
+    REQUIRE(!q.empty());
     q.enqueue(2);
+    REQUIRE(!q.empty());
     q.enqueue(3);
     REQUIRE(!q.empty());
     REQUIRE(*q.dequeue() == 1);
+    REQUIRE(!q.empty());
     REQUIRE(*q.dequeue() == 2);
+    REQUIRE(!q.empty());
     REQUIRE(*q.dequeue() == 3);
     REQUIRE(q.empty());
+  }
+
+  SECTION("3 elements interleaved") {
+    epoch_queue<int> q;
+    REQUIRE(q.empty());
+    q.enqueue(1);
+    REQUIRE(!q.empty());
+    REQUIRE(*q.dequeue() == 1);
+    REQUIRE(q.empty());
+    q.enqueue(2);
+    REQUIRE(!q.empty());
+    q.enqueue(3);
+    REQUIRE(!q.empty());
+    REQUIRE(*q.dequeue() == 2);
+    REQUIRE(!q.empty());
+    REQUIRE(*q.dequeue() == 3);
+    REQUIRE(q.empty());
+  }
+}
+
+void maybe_recieve(epoch_queue<int>& q, unsigned count) {
+  unsigned i = 1;
+  while (i <= count) {
+    //REQUIRE(!q.empty());
+    auto pop = q.dequeue();
+    if (pop) {
+      REQUIRE(*pop == i);
+      i++;
+    }
+  }
+  REQUIRE(q.empty());
+}
+
+TEST_CASE("epoch_queue - two threads") {
+  std::thread t1, t2;
+  SECTION("3 elements") {
+    epoch_queue<int> q;
+    t1 = std::thread([&] {
+      q.enqueue(1);
+      q.enqueue(2);
+      q.enqueue(3);
+    });
+    t2 = std::thread([&] {
+        maybe_recieve(q, 3);
+    });
+    t1.join();
+    t2.join();
   }
 }
 
