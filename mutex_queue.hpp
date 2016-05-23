37c829069f99cae8358e5402f1a6e9e86ccdea26
Neel <Neel012@users.noreply.github.com>
2016-05-23 17:16:27 +0200

    mutex_queue: use std::unique_lock, add empty() function

--- mutex_queue.hpp
+++ mutex_queue.hpp
@@ -10,34 +10,36 @@ namespace lockfree {
     using optional = std::experimental::optional<value_type>;
 
     void enqueue(value_type &value) final {
-      mutex.lock();
+      std::lock_guard<std::mutex> l(mutex);
       queue.push(value);
-      mutex.unlock();
     }
 
     void enqueue(value_type &&value) final {
-      mutex.lock();
+      std::lock_guard<std::mutex> l(mutex);
       queue.push(std::move(value));
-      mutex.unlock();
     }
 
     optional dequeue() final {
       optional result;
 
-      mutex.lock();
+      std::lock_guard<std::mutex> l(mutex);
       if (!queue.empty()) {
         result = queue.front();
         queue.pop();
       }
-      mutex.unlock();
       return result;
+    }
+
+    bool empty() const noexcept final {
+      std::lock_guard<std::mutex> l(mutex);
+      queue.empty();
     }
 
   private:
 
     /* data */
     std::queue<value_type> queue;
-    std::mutex mutex;
+    mutable std::mutex mutex;
   };
 
 
