7c8ea15b1a3b5d4910d3df41c2985654a25d5007
Neel <Neel012@users.noreply.github.com>
2016-06-22 17:39:43 +0200

    tagged_pointer: assert pointer size, endianness, non-lossy storing of pointer

--- tagged_pointer.hpp
+++ tagged_pointer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <atomic>
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
 
@@ -11,8 +12,11 @@ struct tagged_pointer {
   tagged_pointer() = default;
 
   tagged_pointer(T* ptr, uint16_t count = 0) noexcept {
+    static_assert(sizeof(ptr) == sizeof(uint64_t), "Pointer needs to be of size 64 bits");
+    assert(is_little_endian());
     value_.ptr = uint64_t(ptr);
     value_.counter_tag[3] = count;
+    assert(ptr == this->ptr());
   }
 
   T* ptr() const noexcept {
@@ -44,6 +48,12 @@ private:
     uint64_t ptr;
     uint16_t counter_tag[4];
   };
+
+  static bool is_little_endian() {
+    pointer_value e;
+    e.ptr = {0x0002000400060008};
+    return 0x0002 == e.counter_tag[3];
+  }
 
   static const std::uint64_t ptr_mask = 0xffffffffffffUL;
 
